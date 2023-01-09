d38e1baa1d3828be5501c185c87003fe8ecde09e
Cem Mandraci <Cem.Mandraci@bs.nttdata.com>
2023-01-09 12:02:51 +0300

    Created models file and first endpoint

--- MagicVilla.API/Controllers/VillaAPIController.cs
+++ MagicVilla.API/Controllers/VillaAPIController.cs
@@ -1,6 +1,21 @@
-﻿namespace MagicVilla.API.Controllers
+﻿using MagicVilla.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla.API.Controllers
 {
-    public class VillaAPIController
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa { Id=1, Name="Pool View"},
+                new Villa { Id=2, Name="Beach View"}
+            };
+        }
+        
     }
 }
