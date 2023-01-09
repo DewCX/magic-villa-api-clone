689b971c539236278a39c63e0cd544c947bda582
Cem Mandraci <Cem.Mandraci@bs.nttdata.com>
2023-01-09 12:13:51 +0300

    Created Data folder and VillaStore

--- MagicVilla.API/Controllers/VillaAPIController.cs
+++ MagicVilla.API/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
-﻿using MagicVilla.API.Models;
+﻿using MagicVilla.API.Data;
+using MagicVilla.API.Models;
 using MagicVilla.API.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,11 +12,7 @@ namespace MagicVilla.API.Controllers
         [HttpGet]
         public IEnumerable<VillaDTO> GetVillas()
         {
-            return new List<VillaDTO>
-            {
-                new VillaDTO { Id=1, Name="Pool View"},
-                new VillaDTO { Id=2, Name="Beach View"}
-            };
+            return VillaStore.villaList;
         }
         
     }
